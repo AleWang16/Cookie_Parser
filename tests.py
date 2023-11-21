3512156e65ecc88afc55d8c65647e263fb49a350
AleWang16 <66334752+AleWang16@users.noreply.github.com>
2023-11-21 02:59:21 -0500

    Added Comments to Tests

--- tests.py
+++ tests.py
@@ -2,12 +2,19 @@ from cookie_parser import most_common_cookie
 import csv
 
 def empty_sheet(capsys):
+    """
+    Tests empty csv, should return empty string
+    """
     assert cookie_parser.main(['empty.csv', '2019-12-09'])
     
     out, err = capsys.readouterr()
     assert out == ""
 
 def empty_string(capsys):
+    """
+    tests csv where cookies of a given date are not found.  
+    Should return empty string.
+    """
     assert cookie_parser.main(['cookies.csv', '2019-12-07'])
     out, err = capsys.readouterr()
 
@@ -15,11 +22,17 @@ def empty_string(capsys):
     
 
 def one_cookie(capsys):
+    """
+    Tests where there is one cookie that is the most common.
+    """
     assert cookie_parser.main(['cookies.csv', '2019-12-09']) 
     out, err = capsys.readouterr()
     assert out == "D\n"
 
 def various_cookies(capsys):
+    """
+    Tests where there are various cookies that are the most common. 
+    """
     assert cookie_parser.main(['cookies.csv', '2019-12-08'])
     out, err = capsys.readouterr()
     assert out == "A\nC\nD\n"
